a679e0346bc00e5971669d20ad188f94d5edf48c
CynicalBlue <Austin.Whitis04@yahoo.com>
2025-03-16 01:58:39 -0400

    Research Phase

---
+++ Assets/--Scripts--/Tiebreaker/TiebreakerMain.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TiebreakerMain : MonoBehaviour
+{
+    public GameObject player1Pawn;
+    public GameObject player1Silver;
+    public GameObject player1Gold;
+    public GameObject player1Bishop;
+    public GameObject player1Rook;
+    public GameObject player1King;
+    public GameObject player2Pawn;
+    public GameObject player2Silver;
+    public GameObject player2Gold;
+    public GameObject player2Bishop;
+    public GameObject player2Rook;
+    public GameObject player2King;
+
+    public GameObject currentSelectedPiece;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/--Scripts--/Tiebreaker/Tile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
+
+    public void Init(bool isOffset)
+    {
+        _renderer.color = isOffset ? _offsetColor : _baseColor; // If the tile is offset, set the color to the offset color, else set it to the base color
+    }
+
+    void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+    }
+}
